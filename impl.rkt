a386e26cb82d3527e1e81527ddd3fd71716c56c6
Raya Georgieva <raya.georgieva@hotmail.com>
2018-12-06 19:56:15 +0200

    Add implementation

--- impl.rkt
+++ impl.rkt
@@ -7,45 +7,70 @@
 
 (define (id x) x)
 
-; winner returns one of 4 possible things:
-; Either the game hasn't ended, in which case it returns #f
-; Or one of X or O has won, in which case it returns "X" or "O", respectively.
-; Or the game has ended in a draw in which case it returns "D".
-
-; If you are wondering how winner works, read up on
-; "and" and "andmap", on docs.racket-lang.org.
-; Basically everything that is not #f evaluates to #t (like in C++, for example).
-; Furthermore (and "A" "B" 5) will not return #t, because they all evalute to #t,
-; but will instead return the last item in the list.
-; If it encounters an #f instead it will return #f.
-
-; Only detects draws right now.
 (define (winner b)
-  (if (andmap (lambda (xs) (andmap id xs)) b)
-      "D"
-      #f))
+  ; returns a sign if all elements are equal to it
+  ; returns #f if there is at least one #f in the list
+  ; returns "D" if there is no #f in the array and all other elements are not equal
+  (define (has-winner xs)
+    (define candidate (car xs)) 
+    (cond ((not (andmap id xs)) #f)
+          ((all? (lambda (x) (eq? x candidate)) xs) candidate)
+          (else "D")))
 
-; plays is the "ai player". It takes the current board state in curr-board,
-; as a list of lists, i.e.
-; '(("X" #f "O")
-;   (#f "X" #f)
-;   ("O" #f "X"))
-; and what the sign being placed right now is, in curr-sign.
+  (let* ((row (list (rows b)))   ; all rows                         
+         (col (list (cols b)))   ; all cols 
+         (diag (list (diags b))) ; both diagonals
+         (components (concat (append row col diag)))    ; rows + cols + diags
+         (win-comp (map has-winner components)))        ; winners from each component (row, col, diagonal)
+    (cond ((any? (lambda(x) (eq? x "O")) win-comp) "O")
+          ((any? (lambda(x) (eq? x "X")) win-comp) "X")
+          (else (andmap id win-comp)))))
 
-; Board legend:
-; #f - empty spot
-; "X" - an X resides on that spot
-; "O" - an O resides on that spot
 
-; Current sign legend:
-; #t - the current char is an X (you're placing an X)
-; #f - the current char is an O (you're placing an O)
+
+
+; Returns list of pairs each of which represents a possible position that a sign could be placed on board b
+(define (possible-positions b)
+  (define rows (length b))
+  (define cols (length (car b)))
+  (define (help i j)
+    (cond ((>= i rows) '())                                             ; we've reached the end of the matrix
+          ((>= j cols) (help (+ i 1) 0))                                ; we've reached the end of the line
+          ((not (matrix-ref b i j)) (cons (cons i j) (help i (+ j 1)))) ; element is a possible position, so add it to the ist
+          (else (help i (+ j 1)))))                                     ; element is NOT a possible position, so it is not added
+  (help 0 0))
+
+
+; Evalutates a specific position on the given board
+(define (eval-position board position player me)
+  (let* ((row (car position))
+         (col (cdr position))
+         (new-board (place board row col player)) ; board with "player" put on "position"
+         (outcome (winner new-board)))            ; outcome of the game: "X" - X has won, "O" - O has won, "D" - draw, #f game has not still ended
+    (if me
+        (cond ((eq? outcome player) 1)            ; this position is winning for current player
+              ((eq? outcome "D") 0)               ; this position ends up with a draw
+              (else (minimum (map (lambda(position) (eval-position new-board position (oponent player) #f)) (possible-positions new-board)))))
+        (cond ((eq? outcome player) -1)           ; this position is winning for the current player (minimizer)
+              ((eq? outcome "D") 0)
+              (else (maximum (map (lambda(position) (eval-position new-board position (oponent player) #t)) (possible-positions new-board))))))))
+
+; returns max(xs)
+(define (maximum xs)
+  (foldl max (car xs) (cdr xs)))
+
+; returns min(xs)
+(define (minimum xs)
+  (foldl min (car xs) (cdr xs)))
+
+
+; Returns x: f(x) = max{ f(xi) | xi is from xs}
+(define (maximum-on f xs)
+  (foldl (lambda (u v) (if (> (f u) (f v)) u v)) (car xs) (cdr xs)))
+
 
 ; Naive "AI", plays the "next" free spot, going left-to-right, top-to-bottom.
 (define (play curr-board curr-sign)
-  (define (helper i j)
-    (cond ((> i 2) #f)
-          ((> j 2) (helper (+ i 1) 0))
-          ((not (list-ref (list-ref curr-board i) j)) (cons i j))
-          (else (helper i (+ j 1)))))
-  (helper 0 0))
+  (let ((curr-player (if curr-sign "X" "O")))                                                                   ; identify which the current sign is
+    (maximum-on (lambda (pos) (eval-position curr-board pos curr-player #t)) (possible-positions curr-board)))) ; play the best move for current player
+
